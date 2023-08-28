ab8af62af669fae1803edf4420ad08a08a6463bb
Ma Zhiyu <zhiyuma2006@gmail.com>
2023-08-28 21:12:37 +0800

    Initial Commit

---
+++ LifeBot/LifeBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeBotApp.swift
+//  LifeBot
+//
+//  Created by Ma Zhiyu on 28/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LifeBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
